486e50de3c168465cd3e27f0823d27fb291cdb9d
vnbq <foxysroadtech@gmail.com>
2024-09-14 19:15:16 -0500

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,22 @@
+print('Generating random number')
+
+print('Your random number is:')
+
+import random
+Word=random.randint(1,24)
+
+import random
+item=random.randint(3,32)
+
+import random
+word=random.randint(6,23)
+
+import random
+Item=random.randint(8,45)
+
+Item = item + Item
+
+print(Word + item + Item - word)
+
+import time
+time.sleep(1000)
